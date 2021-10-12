3a662f97c99ed344cf07d821ab48d6f6ea1a7292
CharlieDa <charlieda@tedu.tw>
2021-10-13 02:04:25 +0800

    Update settings.py

--- backend/keyboardmarket/keyboardmarket/settings.py
+++ backend/keyboardmarket/keyboardmarket/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w#bn1(bxzmwlmr=wmvklui1)!)3@gntzini%v6i0f&uyj+qac7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
