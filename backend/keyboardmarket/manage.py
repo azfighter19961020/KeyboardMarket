ba0335ba2833a1322b6849faef9ae0b6c02dc05a
CharlieDa <charlieda@tedu.tw>
2021-10-13 02:03:07 +0800

    Update manage.py

--- backend/keyboardmarket/manage.py
+++ backend/keyboardmarket/manage.py
@@ -7,8 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keyboardmarket.settings')
-    os.environ['PAYPAL_CLIENT_ID'] = 'ARAxND323L6wsWMUmG_xneuznOfmjlfYsjhDPccPVggbA5RAzKjmsdx14S-62mdIK84NwINvJgiTY0mI'
-    os.environ['PAYPAL_CLIENT_SECRET'] = 'ECQltltKV0Y3eGhaAgD3dDt8xbwsBTuAdslA8TCXqNxSTjc856_pZ4GXNY_TiX7tuZd4z_8q0R627uvd'
+    os.environ['PAYPAL_CLIENT_ID'] = ''
+    os.environ['PAYPAL_CLIENT_SECRET'] = ''
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
